1dd39beebc9645accbae6136a52ddee1cfd5dfbc
Felipe Rodrigues <rodriguesprado.felipe@gmail.com>
2014-11-15 18:48:31 -0200

    Ajustes na estrutura do projeto.

---
+++ src/controledeentregas/controller/Runner.java
@@ -0,0 +1,10 @@
+package controledeentregas.controller;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		MainController mainController = new MainController();
+		mainController.initialize();
+	}
+
+}
